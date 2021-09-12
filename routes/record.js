648f113b57debf5a4209c0e2d6421daf2dcd0193
MelodRAMAtick <melodramatick@users.noreply.github.com>
2021-09-11 23:41:09 -0300

    Añade endpoints para USUARIO más reordenamiento

--- routes/record.js
+++ routes/record.js
@@ -5,24 +5,12 @@ const express = require("express");
 // The router will be added as a middleware and will take control of requests starting with path /record.
 const recordRoutes = express.Router();
 
-//This will help us connect to the database
+//"Importamos" el código necesario para conectarse al Clúster de MongoDB Atlas
 const dbo = require("../db/conn");
 
-// This section will help you get a list of all the records.
+//-----PRODUCTOS-----
+// Obtener todos los productos.
 recordRoutes.route("/Products/get/all").get(function (req, res) {
-  let db_connect = dbo.getDb("supermercado");
-  db_connect
-    .collection("Venta")
-    .find({})
-    .toArray(function (err, result) {
-      if (err) throw err;
-      res.json(result);
-    });
-});
-
-//Obtener un Producto por su ID
-//TODAVÍA NO DEVUELVE RESULTADOS ESPERADOS
-recordRoutes.route("/Products/get/:id").get(function (req, res) {
   let db_connect = dbo.getDb("supermercado");
   db_connect
     .collection("Producto")
@@ -31,6 +19,69 @@ recordRoutes.route("/Products/get/:id").get(function (req, res) {
       if (err) throw err;
       res.json(result);
     });
+});
+
+//Obtener un Producto por su ID
+recordRoutes.route("/Products/get/:id").get(function (req, res) {
+  let db_connect = dbo.getDb("supermercado");
+  let myquery = { id: req.params.id };
+  db_connect
+    .collection("Producto")
+    .find(myquery)
+    .toArray(function (err, result) {
+      if (err) throw err;
+      res.json(result);
+    });
+});
+
+// Actualizar el stock de un producto según su ID.
+recordRoutes.route("/Products/update/:id").post(function (req, res) {
+  console.log(req.params.id);
+  console.log(req.body.newQuantity);
+  let db_connect = dbo.getDb("supermercado");
+  let myquery = { id: req.params.id };
+  let newvalues = {
+    $set: {
+      stock: req.body.newQuantity,
+    },
+  };
+  db_connect
+    .collection("Producto")
+    .updateOne(myquery, newvalues, function (err, res) {
+      if (err) throw err;
+      console.log("1 document updated");
+    });
+  res.json(res.status + " " + res.statusMessage);
+});
+
+//-------------
+
+//------USUARIOS------
+//Obtener todos los EMPLEADOS ACTIVOS
+recordRoutes.route("/Users/get/all").get(function (req, res) {
+  let db_connect = dbo.getDb("supermercado");
+  // let myquery = { disponible: "si" }
+  db_connect
+    .collection("Usuario")
+    .find({ disponible: "si" })
+    .toArray(function (err, result) {
+      if (err) throw err;
+      res.json(result);
+    });
+});
+
+//Obtener un usuario por su ID.
+recordRoutes.route("/Users/get/:id").get(function (req, res) {
+  let db_connect = dbo.getDb("supermercado");
+  let myquery = { id: req.params.id };
+  db_connect
+    .collection("Usuario")
+    .find(myquery)
+    .toArray(function (err, result) {
+      if (err) throw err;
+      res.json(result);
+    });
+  // console.log(result);
 });
 
 
@@ -47,24 +98,8 @@ recordRoutes.route("/add").post(function (req, res) {
   });
 });
 
-// This section will help you update a record by id.
-recordRoutes.route("/update/:id").post(function (req, res) {
-  let db_connect = dbo.getDb("employees");
-  let myquery = { id: req.body.id };
-  let newvalues = {
-    $set: {
-      person_name: req.body.person_name,
-      person_position: req.body.person_position,
-      person_level: req.body.person_level,
-    },
-  };
-  db_connect
-    .collection("records")
-    .updateOne(myquery, newvalues, function (err, res) {
-      if (err) throw err;
-      console.log("1 document updated");
-    });
-});
+
+
 
 // This section will help you delete a record
 recordRoutes.route("/delete/:id").delete((req, res) => {
