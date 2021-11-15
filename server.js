f8d4ff2a9203e99acd6ff81df804ac1fc04add97
MelodRAMAtick <melodramatick@users.noreply.github.com>
2021-11-15 18:48:49 -0300

    Preflight correction test

--- server.js
+++ server.js
@@ -3,12 +3,11 @@ const app = express();
 const cors = require("cors");
 require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 5000;
-app.use(
-  cors({
-    origin: '*',
-    credentials: true,
-  })
-);app.use(express.json());
+
+
+app.use(cors());
+app.use(express.json());
+
 app.use(require("./routes/record"));
 // get driver connection
 const dbo = require("./db/conn");
