2a04a222dd0be30c67fdb014fa98635d41b21f8a
MelodRAMAtick <melodramatick@users.noreply.github.com>
2021-11-15 00:52:29 -0300

    CORS correction possibility

--- server.js
+++ server.js
@@ -3,8 +3,12 @@ const app = express();
 const cors = require("cors");
 require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 5000;
-app.use(cors());
-app.use(express.json());
+app.use(
+  cors({
+    origin: '*',
+    credentials: true,
+  })
+);app.use(express.json());
 app.use(require("./routes/record"));
 // get driver connection
 const dbo = require("./db/conn");
